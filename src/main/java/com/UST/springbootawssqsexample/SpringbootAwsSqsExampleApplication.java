f5422e42a3ccaaac81ec9a0019117c25424c3725
Samyuktha Sudhakaran <samyukthaes2000@gmail.com>
2023-06-13 09:22:38 +0530

    springboot-aws-sqs-example

---
+++ src/main/java/com/UST/springbootawssqsexample/SpringbootAwsSqsExampleApplication.java
@@ -0,0 +1,13 @@
+package com.UST.springbootawssqsexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAwsSqsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAwsSqsExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/UST/springbootawssqsexample/SpringbootAwsSqsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UST.springbootawssqsexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsSqsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
